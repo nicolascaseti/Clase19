198c4d9eb7f861aa119d10fa79c060488a221f7e
nicolascaseti <nicolas.caseti@gmail.com>
2022-10-05 22:50:08 -0300

    creando clase 20

--- App/views.py
+++ App/views.py
@@ -5,4 +5,7 @@ from App.models import Estudiante
 
 # Create your views here.
 def mostrar_inicio(request):
-    return render(request, "App/inicio.html")
+    estudiante = Estudiante(nombre="Ezequiel", apellido="Ruben", email="a") #Crear un estudiante
+    estudiante.save() #Guarda en la base de datos
+    contexto = {"estudiante_1": estudiante} #Lo va a usar en un contexto
+    return render(request, "App/inicio.html", contexto) #Lo va a pasar en un template
