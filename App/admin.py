f0a07d91f1fb5ee9318be45cc5ade82d955ac14b
nicolascaseti <nicolas.caseti@gmail.com>
2022-10-06 15:19:56 -0300

    creando la entidad Curso en el servidor

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,7 @@
+from App.models import Estudiante, Curso
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Estudiante)
+admin.site.register(Curso)
--- App/models.py
+++ App/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Curso(models.Model):
+    nombre = models.CharField(max_length=30)
+    camada = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return f"({self.camada}) {self.nombre}"
+
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
+    
+    
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
+    
+    
+    #Este metodo __str__ servia para poder definir como se iba a mostrar por consola una clase
